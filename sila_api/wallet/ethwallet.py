7b2fa27af4e5e7559828ca2cee4bdcfee056469c
sila <deepanshu179@gmail.com>
2019-01-23 14:59:44 -0800

    changes

--- sila_api/wallet/ethwallet.py
+++ sila_api/wallet/ethwallet.py
@@ -1,6 +1,9 @@
-from web3.auto import web3
+from web3.auto import w3
 
 
 def createEthWallet(pass_phrase):
-    pass
+    
+    account=w3.eth.account.create(pass_phrase)
+    
+    return {"private_key":account.privateKey,"eth_address":account.address}
     
