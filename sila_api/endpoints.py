7a0e82b09aa7a411e1be80a911e96b9a38cb6f3f
sila <deepanshu179@gmail.com>
2019-01-29 12:32:33 -0800

    changes

--- sila_api/endpoints.py
+++ sila_api/endpoints.py
@@ -1,7 +1,20 @@
 path={
-    "createEntity":"/create_entity",
-    "verifyAccount":"/verify_account",
-    "issueSila"    :"/issue",
-    "redeemSila"   :"/redeem",
-    "transferSila" : "/transfer"
+    "createEntity"     : "/create_entity",
+    "verifyAccount"    : "/verify_account",
+    "issueSila"        : "/issue_sila",
+    "redeemSila"       : "/redeem_sila",
+    "transferSila"     : "/transfer_sila",
+    "addCrypto"        : "/add_crypto",
+    "addIdentity"      : "/add_identity",
+    "createApp"        : "/create_app",
+    "createBond"       : "/create_bond",
+    "checkHandle"      : "/check_handle",
+    "getAccounts"      : "/get_accounts",
+    "getApps"          : "/get_apps",
+    "getProfile"       : "/get_profile",
+    "getUsers"         : "/get_users",
+    "registerOperator" : "/register_operator",
+    "linkAccount"      : "/link_account"
+
+
 }
