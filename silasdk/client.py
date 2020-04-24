e3440bd910c5bd68627e9c28bfa6500a15d597bb
Carlos Sosa <csosa@bytesw.com>
2020-04-24 16:50:20 -0600

    Update schema functionalities for use on new endpoints

--- silasdk/client.py
+++ silasdk/client.py
@@ -11,9 +11,8 @@ from .schema import Schema
 # basic client for making http requests like post,get etc
 
 class App():
-    
-    
-    def __init__(self,tier,app_private_key,app_handle):
+
+    def __init__(self, tier, app_private_key, app_handle):
         """Initalize the application 
             This lets users initialize the application by providing the tier, application privatekey and application handle
         Args:
@@ -21,103 +20,110 @@ class App():
             app_private_key : ethereum privat key for the application
             app_handle  : application sila handle (app.silamoney.eth)
         """
-        self.session=requests.Session()
-        self.tier=tier.lower()
-        self.app_private_key=app_private_key
-        self.app_handle=app_handle
+        self.session = requests.Session()
+        self.tier = tier.lower()
+        self.app_private_key = app_private_key
+        self.app_handle = app_handle
         self.updateSchema()
 
-        
-    
     def updateSchema(self):
         """updates schema.py on initialization of app
             This lets users initialize the schema into schema.py for ease of use
         Args:
             None
         """
-        endpoint=endPoints["schemaUrl"]
-        message=["header","issue","redeem","transfer","entity","identity","crypto","linkAccount"]
+        endpoint = endPoints["schemaUrl"]
+        message = ["header", "issue", "redeem", "transfer", "entity", "identity", "crypto", "linkAccount"]
         for i in message:
-            response=self.get(
-                    endpoint %i)
-            Schema.append(response)
-        
-            
+            response = self.get(
+                endpoint % i)
 
+            sch = {response["message"]: response}
 
-          
+            Schema.append(sch)
+
     def getUrl(self):
         """construct the url endpoint to make api calls
         Args:
             app: the initialized applications
         """
-        url=endPoints["apiUrl"]
-        if self.tier=="prod":
-            apiurl=url %"api"
+        url = endPoints["apiUrl"]
+        if self.tier == "prod":
+            apiurl = url % "api"
         else:
-            apiurl=url %self.tier
+            apiurl = url % self.tier
 
         return apiurl
 
-        
-
-    def post(self,path,payload,header):
+    def post(self, path, payload, header):
         """makes a post request to the sila_apis
         Args:
             path : path to the endpoint being called
             payload : json msg to be posted 
             header  : contains the usersignature and authsignature
         """
-        url = self.getUrl() 
-        endpoint=url + path
-        data1=json.dumps(payload)
+        url = self.getUrl()
+        endpoint = url + path
+        data1 = json.dumps(payload)
         response = self.session.post(
-                endpoint,
-                data=data1,
-                headers=header)
-        
+            endpoint,
+            data=data1,
+            headers=header)
+
         # output=self.checkResponse(response)
-        output=yaml.safe_load(json.dumps(response.json()))
+        output = yaml.safe_load(json.dumps(response.json()))
         return output
 
+    def postPlaid(self, url, payload):
+        """makes a post request to the sila_apis
+        Args:
+            path : path to the endpoint being called
+            payload : json msg to be posted
+            header  : contains the usersignature and authsignature
+        """
+        content = json.dumps(payload)
+        response = self.session.post(
+            url,
+            data=content,
+            headers={
+                "Content-Type": "application/json"
+            })
 
+        # output=self.checkResponse(response)
+        output = yaml.safe_load(json.dumps(response.json()))
+        return output
 
-    def get(self,path):
+    def get(self, path):
         """make a get request using this function
         Args:
             path : path to the endpoint
         """
         endpoint = path
-        response =self.session.get(endpoint)
-        output=yaml.safe_load(json.dumps(response.json()))
+        response = self.session.get(endpoint)
+        output = yaml.safe_load(json.dumps(response.json()))
         return output
 
-    
-
-    def setHeader(self,msg,key=None):
+    def setHeader(self, msg, key=None):
         """set the application header with usersignature and authsignature
         Args:
             key : ethereum private key for the user
             msg : message being sent should be signed by user
         """
-        appsignature=EthWallet.signMessage(msg,self.app_private_key)
-        if key!=None:
-            usersignature=EthWallet.signMessage(msg,key.lower())
-            header={
+        appsignature = EthWallet.signMessage(msg, self.app_private_key)
+        if key != None:
+            usersignature = EthWallet.signMessage(msg, key.lower())
+            header = {
                 'Content-Type': 'application/json',
                 "usersignature": usersignature,
-                "authsignature":  appsignature
+                "authsignature": appsignature
             }
             return header
         else:
-            header={
+            header = {
                 'Content-Type': 'application/json',
-                "authsignature":  appsignature
+                "authsignature": appsignature
             }
             return header
-           
-
-
 
     def checkResponse(self, response):
         """	Takes the returned JSON result from sila apis and checks it for odd errors, returning the response
@@ -126,35 +132,20 @@ class App():
             Parameters:
                 response: A JSON object returned from Authentic Jobs or n error
         """
-        output=yaml.safe_load(json.dumps(response.json()))
+        output = yaml.safe_load(json.dumps(response.json()))
 
         if response.status_code == 200:
-            
+
             return output
 
         else:
             print(response.status_code)
             try:
-                for k,v in Errors.items():
-                    if response.status_code==k:
-                        return {"error_msg":str(v)}
+                for k, v in Errors.items():
+                    if response.status_code == k:
+                        return {"error_msg": str(v)}
             except:
-                return {"error_msg":"something_went_wrong"}
+                return {"error_msg": "something_went_wrong"}
 
-
-
-    
     def log(self):
         pass
-
-
-   
-
-
-    
-
-
-   
-
-
-   
--- silasdk/schema.py
+++ silasdk/schema.py
@@ -1,1 +1,136 @@
-Schema=[]
+Schema = [
+    {"header_msg_kyc_level": {
+        "header": {
+            "reference": "",
+            "created": 1587419132,
+            "user_handle": "",
+            "auth_handle": "",
+            "version": "0.2",
+            "crypto": "ETH"
+        },
+        "message": "header_msg_kyc_level",
+        "kyc_level": "CUSTOM_KYC_FLOW_NAME"
+    }},
+    {"link_account_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "message": "link_account_msg",
+        "public_token": "public-xxx-xxx",
+        "account_name": "Custom Account Name",
+        "selected_account_id": "optional_selected_account_id"
+    }},
+    {"link_account_msg_plaid": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "message": "link_account_msg_plaid",
+        "account_number": "123456789012",
+        "routing_number": "123456789",
+        "account_type": "CHECKING",
+        "account_name": "Custom Account Name",
+        "selected_account_id": "0"
+    }},
+    {"account_name_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+        },
+        "account_name": "Custom Account Name"
+    }},
+    {"transfer_msg_address": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "message": "transfer_msg_address",
+        "amount": 13,
+        "destination": "user2.silamoney.eth",
+        "destination_address": "user2.silamoney.eth"
+    }},
+    {"no_content_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        }
+    }},
+    {"update_wallet_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "nickname": "new_wallet_nickname",
+        "default": True
+    }},
+    {"register_wallet_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "wallet_verification_signature": "(signature generated from signing address as the message)",
+        "wallet": {
+            "blockchain_address": "(address to register to user_handle)",
+            "blockchain_network": "ETH",
+            "nickname": "new_wallet_nickname"
+        }
+    }},
+    {"get_wallets_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "search_filters": {
+            "page": 1,
+            "per_page": 20,
+            "sort_ascending": False,
+            "blockchain_network": "ETH",
+            "blockchain_address": "",
+            "nickname": ""
+        }
+    }},
+    {"get_accounts_msg": {
+        "header": {
+            "created": 1234567890,
+            "auth_handle": "handle.silamoney.eth",
+            "user_handle": "user.silamoney.eth",
+            "version": "0.2",
+            "crypto": "ETH",
+            "reference": "ref"
+        },
+        "message": "get_accounts_msg"
+    }},
+    {"sila_balance_msg": {
+        "address": "0x0000"
+    }}
+]
