894c4ee05a570c163b1059dcb282fb0cc9be7d51
Carlos Sosa <csosa@bytesw.com>
2020-04-24 16:47:27 -0600

    Add new and update sila endpoints

--- silasdk/users.py
+++ silasdk/users.py
@@ -1,18 +1,12 @@
 from .endpoints import endPoints
 from silasdk import message
-from .ethwallet import EthWallet
-import time
 import json
 import requests
 import yaml
 
 
-
 class User():
-
-
-
-    def checkHandle(self,payload):
+    def checkHandle(self, payload):
         """Check if the user handle is available.
         These endpoint returns the validity of a user handle
         Args:
@@ -20,13 +14,12 @@ class User():
         Returns:
         dict: response body (a confirmation message)
         """
-        path=endPoints["checkHandle"]
-        msg_type="header_msg"
-        response=message.postRequest(self,path,msg_type,payload)        
+        path = endPoints["checkHandle"]
+        msg_type = "header_msg"
+        response = message.postRequest(self, path, msg_type, payload)
         return response
 
-
-    def register(self,payload):
+    def register(self, payload):
         """Register a new user.
            This user will be kyced and ethereum address will be registered with sila 
         Args:
@@ -35,12 +28,11 @@ class User():
             dict: response body (a confirmation message)
         """
         path = endPoints["register"]
-        msg_type="entity_msg"
-        response=message.postRequest(self,path,msg_type,payload)        
+        msg_type = "entity_msg"
+        response = message.postRequest(self, path, msg_type, payload)
         return response
 
-
-    def requestKyc(self,payload,user_private_key):
+    def requestKyc(self, payload, user_private_key, use_kyc_level=False):
         """Request kyc for a user by handle
            This user will be kyced and ethereum address will be registered with sila 
         Args:
@@ -48,12 +40,12 @@ class User():
         Returns:
             dict: response body (a confirmation message)
         """
-        path=endPoints["requestKyc"]
-        msg_type="header_msg"
-        response=message.postRequest(self,path,msg_type,payload,user_private_key)        
+        path = endPoints["requestKyc"]
+        msg_type = ("header_msg" if (use_kyc_level is False) else "header_msg_kyc_level")
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
         return response
-    
-    def linkAccount(self,payload,user_private_key):
+
+    def linkAccount(self, payload, user_private_key, plaid=False):
         """link the bank account of user using plad
            This users bank account will be linked  
         Args:
@@ -63,13 +55,11 @@ class User():
             dict: response body (a confirmation message)
         """
         path = endPoints["linkAccount"]
-        msg_type="link_account_msg"
-        response=message.postRequest(self,path,msg_type,payload,user_private_key)        
+        msg_type = ("link_account_msg" if (plaid is False) else "link_account_msg_plaid")
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
         return response
-       
 
-        
-    def checkKyc(self,payload,user_private_key):
+    def checkKyc(self, payload, user_private_key):
         """check if the user has been kyced.
             The user will be checked if the they have been kyced
         Args:
@@ -77,27 +67,26 @@ class User():
         Returns:
             dict: response body (a confirmation message)
         """
-        path=endPoints["checkKyc"]
-        msg_type="header_msg"
-        response=message.postRequest(self,path,msg_type,payload,user_private_key)        
-        return response
-    
-    def addCrypto(self,payload,user_private_key):
-        """check if the user has been kyced.
-            The used will be checked if the they have been kyced
-        Args:
-            payload : includes the crypto adddress, handle etc that need to be added
-            user_private_key: users ethereum private key 
-        Returns:
-            dict: response body (a confirmation message)
-        """
-        path=endPoints["addCrypto"]
-        msg_type="crypto_msg"
-        response=message.postRequest(self,path,crypto,payload,user_private_key)        
+        path = endPoints["checkKyc"]
+        msg_type = "header_msg"
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
         return response
 
+    # def addCrypto(self, payload, user_private_key):
+    #     """check if the user has been kyced.
+    #         The used will be checked if the they have been kyced
+    #     Args:
+    #         payload : includes the crypto adddress, handle etc that need to be added
+    #         user_private_key: users ethereum private key
+    #     Returns:
+    #         dict: response body (a confirmation message)
+    #     """
+    #     path = endPoints["addCrypto"]
+    #     msg_type = "crypto_msg"
+    #     response = message.postRequest(self, path, crypto, payload, user_private_key)
+    #     return response
 
-    def addIdentity(self,payload,user_private_key):
+    def addIdentity(self, payload, user_private_key):
         """change the info about user like change ssn, email ,etc.
             The used will be checked if the they have been kyced
         Args:
@@ -106,14 +95,12 @@ class User():
         Returns:
             dict: response body (a confirmation message)
         """
-        path=endPoints["addIdentity"]
-        msg_type="identity_msg"
-        response=message.postRequest(self,path,msg_type,payload,user_private_key)        
+        path = endPoints["addIdentity"]
+        msg_type = "identity_msg"
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
         return response
-    
 
-   
-    def  getAccounts(self,payload,user_private_key):
+    def getAccounts(self, payload, user_private_key):
         """get the accounts of users registered with sila
             The user will be checked if they have been kyced, along with app
         Args:
@@ -122,14 +109,25 @@ class User():
         Returns:
             dict: response body (a confirmation message)
         """
-        path=endPoints["getAccounts"]
-        msg_type="header_msg"
-        response=message.postRequest(self,path,msg_type,payload,user_private_key)        
+        path = endPoints["getAccounts"]
+        msg_type = "get_accounts_msg"
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
         return response
 
+    def getAccountBalance(self, payload, user_private_key):
+        """get the account balance of a user registered with sila
+        Args:
+            user_hanlde: users handle registered with app
+            user_private_key: user private key asscoicated with crypto address
+        Returns:
+            dict: response body (a confirmation message)
+        """
+        path = endPoints["getAccountBalance"]
+        msg_type = "account_name_msg"
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
+        return response
 
-
-    def  getTransactions(self,payload,user_private_key):
+    def getTransactions(self, payload, user_private_key):
         """get the users transactions registered with ur app
            The user will be checked if they have been kyced, along with app
         Args:
@@ -138,12 +136,12 @@ class User():
         Returns:
             dict: response body (a confirmation message)
         """
-        path=endPoints["getTransactions"]
-        msg_type="header_msg"
-        response=message.postRequest(self,path,msg_type,payload,user_private_key)        
+        path = endPoints["getTransactions"]
+        msg_type = "header_msg"
+        response = message.postRequest(self, path, msg_type, payload, user_private_key)
         return response
-    
-    def silaBalance(self,address):
+
+    def silaBalance(self, address):
         """get sila balance of the addresses registered with sila
            The user will be checked if they have been kyced, along with app
         Args:
@@ -151,29 +149,11 @@ class User():
         Returns:
             dict: response body (a confirmation message)
         """
-        data=json.dumps({"address":str(address)})
-        header={'content-type': 'application/json'}
-        if self.tier=="prod":
-            endpoint=endPoints["silaBalanceProd"]
-        elif self.tier=="sandbox":
-            endpoint=endPoints["silaBalanceSandbox"]
-        response=requests.post(endpoint,data=data,headers=header)
-        return (yaml.safe_load(json.dumps(response.json())))
-
-
-
-
-    
-    
-     
-    
-
-
-    
-
-        
-        
-
-
-
-        
+        data = json.dumps({"address": str(address)})
+        header = {'content-type': 'application/json'}
+        if self.tier == "prod":
+            endpoint = endPoints["silaBalanceProd"]
+        elif self.tier == "sandbox":
+            endpoint = endPoints["silaBalanceSandbox"]
+        response = requests.post(endpoint, data=data, headers=header)
+        return yaml.safe_load(json.dumps(response.json()))
