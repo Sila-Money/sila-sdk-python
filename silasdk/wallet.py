b058e413ba47666136ccf82438f24960f86c3cac
chepix10 <jlmruiz@outlook.com>
2020-12-14 09:47:34 -0600

    Added tests for /update_wallet

--- silasdk/wallet.py
+++ silasdk/wallet.py
@@ -1,7 +1,10 @@
 from .endpoints import endPoints
 from silasdk import message
+from silasdk.client import App
 import json
 import requests
+import warnings
+
 
 class Wallet():
     def registerWallet(self, payload, user_private_key):
@@ -13,7 +16,8 @@ class Wallet():
         """
         path = endPoints["registerWallet"]
         msg_type = "register_wallet_msg"
-        response = message.postRequest(self, path, msg_type, payload, user_private_key)
+        response = message.postRequest(
+            self, path, msg_type, payload, user_private_key)
         return response
 
     def getWallets(self, payload, user_private_key):
@@ -25,7 +29,8 @@ class Wallet():
         """
         path = endPoints["getWallets"]
         msg_type = "get_wallets_msg"
-        response = message.postRequest(self, path, msg_type, payload, user_private_key)
+        response = message.postRequest(
+            self, path, msg_type, payload, user_private_key)
         return response
 
     def getWallet(self, payload, user_private_key):
@@ -37,19 +42,38 @@ class Wallet():
         """
         path = endPoints["getWallet"]
         msg_type = "no_content_msg"
-        response = message.postRequest(self, path, msg_type, payload, user_private_key)
+        response = message.postRequest(
+            self, path, msg_type, payload, user_private_key)
         return response
 
-    def updateWallet(self, payload, user_private_key):
+    @staticmethod
+    def updateWallet(app: App, payload: dict, user_private_key: str) -> dict:
         """Update a wallet
         Args:
-        payload : Required user_handle to check if its available
+            app (App): The app configuration object
+            payload (dict) : Required user_handle to check if its available
+            user_private_key (str): The user handle's private key to sign the request
         Returns:
-        dict: response body (a confirmation message)
+            dict: response body (a confirmation message)
+        """
+        warnings.warn(
+            'This method has been deprecated in favor of update_wallet', DeprecationWarning)
+        return Wallet.update_wallet(app, payload, user_private_key)
+
+    @staticmethod
+    def update_wallet(app: App, payload: dict, user_private_key: str) -> dict:
+        """Update a wallet
+        Args:
+            app (App): The app configuration object
+            payload (dict) : Required user_handle to check if its available
+            user_private_key (str): The user handle's private key to sign the request
+        Returns:
+            dict: response body (a confirmation message)
         """
         path = endPoints["updateWallet"]
         msg_type = "update_wallet_msg"
-        response = message.postRequest(self, path, msg_type, payload, user_private_key)
+        response = message.postRequest(
+            app, path, msg_type, payload, user_private_key)
         return response
 
     def deleteWallet(self, payload, user_private_key):
@@ -61,5 +85,6 @@ class Wallet():
         """
         path = endPoints["deleteWallet"]
         msg_type = "no_content_msg"
-        response = message.postRequest(self, path, msg_type, payload, user_private_key)
+        response = message.postRequest(
+            self, path, msg_type, payload, user_private_key)
         return response
--- tests/test017_update_wallet.py
+++ tests/test017_update_wallet.py
@@ -1,41 +1,49 @@
-import unittest, silasdk
+import unittest
+from silasdk.wallet import Wallet
 
-from tests.test_config import *
+from tests.test_config import (app, user_handle, eth_private_key)
+
 
 class Test017UpdateWalletTest(unittest.TestCase):
     def test_update_wallet_200(self):
+        nickname = 'wallet_python_updated'
         payload = {
             "user_handle": user_handle,
-            "nickname": "wallet_python_updated",
-            "crypto": "ETH",
+            "nickname": nickname,
             "default": True
         }
 
-        response = silasdk.Wallet.getWallet(app, payload, eth_private_key)
-        self.assertTrue(response["success"])
+        response = Wallet.update_wallet(app, payload, eth_private_key)
+        self.assertTrue(response.get('success'))
+        self.assertEqual(response.get('status_code'), 200)
+        self.assertEqual(response.get('status'), 'SUCCESS')
+        self.assertIsNotNone(response.get('wallet'))
+        self.assertEqual(response.get('wallet').get(
+            'nickname'), nickname)
+        self.assertTrue(response.get('wallet').get('default'))
 
     def test_update_wallet_400(self):
         payload = {
-            "user_handle": user_handle,
-            "nickname": "wallet_python_updated",
-            "default": True,
-            "crypto": "ETHS",
-            "blockchain_address": "0x00000"
+            "user_handle": "",
+            "nickname": ""
         }
 
-        response = silasdk.Wallet.getWallet(app, payload, eth_private_key)
-        self.assertFalse(response["success"])
+        response = Wallet.update_wallet(app, payload, eth_private_key)
+        self.assertFalse(response.get('success'))
+        self.assertEqual(response.get('status_code'), 400)
+        self.assertEqual(response.get('status'), 'FAILURE')
 
     def test_update_wallet_401(self):
         payload = {
-            "user_handle": "",
+            "user_handle": user_handle,
             "nickname": "wallet_python_updated",
-            "crypto": "ETH",
             "default": True
         }
 
-        response = silasdk.Wallet.getWallet(app, payload, eth_private_key)
-        self.assertFalse(response["success"])
+        response = Wallet.update_wallet(app, payload, '')
+        self.assertFalse(response.get('success'))
+        self.assertEqual(response.get('status_code'), 403)
+        self.assertEqual(response.get('status'), 'FAILURE')
 
 
 if __name__ == '__main__':
