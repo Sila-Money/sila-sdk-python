729f0fa1ea43d4db328f4c34def83e40bd085f2b
Arcgate-shubhamk <shubham.kumawat@gmail.com>
2023-06-26 17:31:34 +0530

    whitespace removed

--- tests/test005_check_kyc.py
+++ tests/test005_check_kyc.py
@@ -27,30 +27,30 @@ class Test005CheckKycTest(unittest.TestCase):
             "kyc_level": "INSTANT-ACHV2"
         }
         response_4 = User.checkKyc(app, payload, eth_private_key_6)
-        # while response["status"] != "SUCCESS" or response_2["status"] != "SUCCESS" or \
-        #       response_4["status"] != "SUCCESS" or "Business has passed verification" not in response_3["message"]:
-        #     time.sleep(100)
-        #     payload = {
-        #         "user_handle": user_handle
-        #     }
-        #     response = User.checkKyc(app, payload, eth_private_key)
+        while response["status"] != "SUCCESS" or response_2["status"] != "SUCCESS" or \
+              response_4["status"] != "SUCCESS" or "Business has passed verification" not in response_3["message"]:
+            time.sleep(100)
+            payload = {
+                "user_handle": user_handle
+            }
+            response = User.checkKyc(app, payload, eth_private_key)
 
-        #     payload = {
-        #         "user_handle": user_handle_2
-        #     }
-        #     response_2 = User.checkKyc(app, payload, eth_private_key_2)
+            payload = {
+                "user_handle": user_handle_2
+            }
+            response_2 = User.checkKyc(app, payload, eth_private_key_2)
 
-        #     payload = {
-        #         "user_handle": business_handle
-        #     }
-        #     response_3 = User.checkKyc(app, payload, eth_private_key_3)
+            payload = {
+                "user_handle": business_handle
+            }
+            response_3 = User.checkKyc(app, payload, eth_private_key_3)
 
-        #     payload = {
-        #         "user_handle": sardine_handle,
-        #         "kyc_level": "INSTANT-ACHV2"
-        #     }
-        #     response_4 = User.checkKyc(app, payload, eth_private_key_6)
-        # self.assertEqual(response["status"], "SUCCESS")
+            payload = {
+                "user_handle": sardine_handle,
+                "kyc_level": "INSTANT-ACHV2"
+            }
+            response_4 = User.checkKyc(app, payload, eth_private_key_6)
+        self.assertEqual(response["status"], "SUCCESS")
 
 
 if __name__ == '__main__':
