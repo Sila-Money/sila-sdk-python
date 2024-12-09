6f572bc2a22be469c22c8394eb2c82fb4ef7c56e
Heather Johnson <heather.johnson@silamoney.com>
2024-12-09 09:26:45 -0800

    ENGRG-4781 removed commented out test.

--- tests/test006_link_account.py
+++ tests/test006_link_account.py
@@ -62,51 +62,6 @@ class Test006LinkAccountTest(unittest.TestCase):
 
         self.assertEqual(response["status"], "SUCCESS", msg=response.get('message', 'No message provided'))
 
-    # def test_link_account_plaid_200(self):
-    #     options = {
-    #         "public_key": "fa9dd19eb40982275785b09760ab79",
-    #         "initial_products": ["transactions"],
-    #         "institution_id": "ins_109508",
-    #         "credentials": {
-    #             "username": "user_good",
-    #             "password": "pass_good"
-    #         }
-    #     }
-    #
-    #     plaid_response = app.postPlaid(
-    #         "https://sandbox.plaid.com/link/item/create", options)
-    #     payload = {
-    #         "user_handle": user_handle,
-    #         "account_name": "default_plaid",
-    #         "public_token": plaid_response["public_token"],
-    #         "selected_account_id": plaid_response["accounts"][0]["account_id"],
-    #         "message": "link_account_msg"
-    #     }
-    #
-    #     response = silasdk.User.linkAccount(
-    #         app, payload, eth_private_key, True)
-    #
-    #
-    #     self.assertEqual(response["status"], "SUCCESS", msg=response.get('message', 'No message provided'))
-    #     self.assertIsNotNone(response['account_owner_name'])
-    #
-    #     plaid_response = app.postPlaid(
-    #         "https://sandbox.plaid.com/link/item/create", options)
-    #     payload = {
-    #         "user_handle": user_handle,
-    #         "account_name": "default_plaid2",
-    #         "plaid_token": plaid_response["public_token"],
-    #         "message": "link_account_msg",
-    #         "plaid_token_type": "legacy"
-    #     }
-    #
-    #     response = silasdk.User.linkAccount(
-    #         app, payload, eth_private_key, True)
-    #
-    #
-    #     self.assertEqual(response["status"], "SUCCESS", msg=response.get('message', 'No message provided'))
-    #     self.assertIsNotNone(response['account_owner_name'])
-
     def test_link_account_400(self):
         payload = {
             "account_name": "default",
