171563898bbf32d5c1ba9086157b928bfb1ff6c2
Pamela Jane <pamela.jane@silamoney.com>
2025-08-11 18:26:42 -0700

    ENGRG-6180 Guard setup.py under __main__ to prevent setuptools hijacking tests

--- setup.py
+++ setup.py
@@ -2,56 +2,58 @@ from setuptools import setup, find_packages
 
 CURRENT_VERSION = '1.1.3'
 
+def _setup():
+    setup(
+        name='silasdk',
 
-setup(
-    name='silasdk',
+        version=CURRENT_VERSION,
 
-    version=CURRENT_VERSION,
+        description='Sila Python library for message signing and api wrapper',
 
-    description='Sila Python library for message signing and api wrapper',
+        url="https://github.com/Sila-Money/Sila-Python",
 
-    url="https://github.com/Sila-Money/Sila-Python",
+        author='Sila',
+        author_email='support@silamoney.com',
 
-    author='Sila',
-    author_email='support@silamoney.com',
+        license='Apache',
 
-    license='Apache',
+        classifiers=[
 
-    classifiers=[
+            # How stable is your project?
+            #   3 - Alpha, 4 - Beta,5 - Production/Stable
 
-        # How stable is your project?
-        #   3 - Alpha, 4 - Beta,5 - Production/Stable
+            'Development Status :: 5 - Production/Stable',
 
-        'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
 
-        'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
 
-        'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.12'
 
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.12'
+        ],
 
-    ],
+        keywords=f'Sila v{CURRENT_VERSION} Rest API',
 
-    keywords=f'Sila v{CURRENT_VERSION} Rest API',
+        packages=find_packages(exclude=["tests", "tests.*"]),
 
-    packages=find_packages(exclude=["tests", "tests.*"]),
+        install_requires=[
+            "requests==2.32.4",
+            "pyaml>=24.9.0",
+            "eth-account==0.13.7",
+            "web3==7.13.0",
+        ],
 
-    install_requires=[
-        "requests==2.32.4",
-        "pyaml>=24.9.0",
-        "eth-account==0.13.7",
-        "setuptools>=75.6.0",
-        "web3==7.13.0"
-    ],
+        zip_safe=False,
 
-    zip_safe=False,
+        include_package_data=True,
 
-    include_package_data=True,
+        extras_require={},
 
-    extras_require={},
+        package_data={},
 
-    package_data={},
+        entry_points={}
+    )
 
-    entry_points={}
-)
+if __name__ == "__main__":
+    _setup()
